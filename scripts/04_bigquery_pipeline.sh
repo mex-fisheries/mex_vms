0644e8173b8cc7ff10053f6e25196e39c4570277
jcvdav <juancarlos.villader@gmail.com>
2026-04-09 11:02:14 -0400

    Updating pipeline to upload 2024 and 2025 data

--- scripts/04_bigquery_pipeline.sh
+++ scripts/04_bigquery_pipeline.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-bq query --use_legacy_sql=false < pipeline_1_segmenter.sql
-bq query --use_legacy_sql=false < pipeline_2_segment_info.sql
+bq query --use_legacy_sql=false < scripts/pipeline_1_segmenter.sql
+bq query --use_legacy_sql=false < scripts/pipeline_2_segment_info.sql
 
-date >> ../../data/mex_vms/bq_pipeline.log
